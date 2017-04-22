3b245a4c9096456382906a258b8bb65abd69730e
Peter Schueller <schueller.p@gmail.com>
2017-04-22 10:40:28 +0300

    copyright update

--- src/shallowhexparser.py
+++ src/shallowhexparser.py
@@ -1,9 +1,23 @@
+# This parser should be able to parse most ASP-related languages, including HEX and ASP-Core-2.
+
+# HEXLite Python-based solver for a fragment of HEX
+# Copyright (C) 2017  Peter Schueller <schueller.p@gmail.com>
 #
-# author: Peter Schuller <schueller.p@gmail.com>
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
-this parser can be used as follows:
+The parser can be used as follows:
 
 import shallowhexparser
 structure = shallowhexparser.parse(text)
