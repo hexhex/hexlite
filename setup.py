94904209a35f20cf7966b55c9b7a2af12db9c993
Peter Schueller <schueller.p@gmail.com>
2017-05-31 22:40:43 +0300

    uploading to pypi works

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python2
+
+from setuptools import setup
+
+# TODO dependency clingo?
+
+def readme():
+  with open('README.rst') as f:
+    return ''.join([ line for line in f if not line.startswith('[')])
+
+setup(name='hexlite',
+      version='0.3.1',
+      description='HEXLite Python-based solver for a fragment of HEX',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 2.7',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Utilities',
+      ],
+      url='https://github.com/hexhex/hexlite',
+      author='Peter Schuller',
+      author_email='schueller.p@gmail.com',
+      license='GPL3',
+      packages=['hexlite', 'dlvhex'],
+      install_requires=[
+        'ply',
+      ],
+      scripts=['bin/hexlite'],
+      zip_safe=False)
