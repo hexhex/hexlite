ce1fb094b197325c8b2002c1e561c9d718915a4f
Peter Schueller <schueller.p@gmail.com>
2018-05-27 17:16:54 +0200

    action manager minimal implementation

--- acthex/actionmanager.py
+++ acthex/actionmanager.py
@@ -1,5 +1,6 @@
 
 import dlvhex
+import acthex
 
 import hexlite.aux as aux
 import hexlite.ast.shallowparser as shp
@@ -8,23 +9,47 @@ import logging
 
 class ActionToBeExecuted:
   def __init__(self, name, args, prio):
+    assert(isinstance(name, str))
     self.name = name
-    self.args = args
+    assert(isinstance(args, tuple))
+    assert(all([isinstance(arg, dlvhex.ID) for arg in args]))
+    self.arguments = args
+    assert(isinstance(prio, int))
     self.prio = prio
+  def __repr__(self):
+    return str(self)
+  def __str__(self):
+    return "@{}({}){{{}}}".format(self.name, ','.join([str(x) for x in self.arguments]), self.prio)
 
 def extractActions(model):
   def parseAction(a):
     assert(isinstance(a, dlvhex.ID))
-    aname, aargs, aprio = a.tuple()
-    aname = aname[len(aux.Aux.ACTREPL):]
+    atuple = a.tuple()
+    aname, aprio = atuple[0].value(), atuple[2].intValue()
+    aargs = atuple[1].tuple()[1:]
+    aname = aname[len(aux.Aux.ACTREPL)+1:]
     logging.debug("from ID={} extracted name={} args={} prio={}".format(repr(a), repr(aname), repr(aargs), repr(aprio)))
     return ActionToBeExecuted(aname, aargs, aprio)
   return [ parseAction(a) for a in model.atoms if a.value().startswith(aux.Aux.ACTREPL) ]
+
+def buildSchedule(actions):
+  return sorted(actions, key=lambda action: action.prio)
+
+def executeAction(action):
+  aname = action.name
+  if aname not in acthex.actions:
+    raise KeyError("action name {} of action {} was not registered with acthex!".format(aname, repr(action)))
+  aholder = acthex.actions[aname]
+  # TODO check aholder.inspec
+  aholder.func(*action.arguments)
 
 def executeActions(model):
   # extract actions from model
   actions = extractActions(model)
   logging.debug("extracted actions: %s", actions)
   # build schedule
+  schedule = buildSchedule(actions)
+  logging.info("action schedule: %s", schedule)
   # execute on environment
-  raise Exception("not implemented")
+  for action in schedule:
+    executeAction(action)
