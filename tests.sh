5006fa6ace1b3c802ba6b908f3019138cb4f9cc9
Peter Schueller <contact@peterschueller.com>
2019-10-28 21:47:32 +0100

    deactivating acthex tests for development

--- tests.sh
+++ tests.sh
@@ -7,27 +7,30 @@ hexlite $EXTRA \
   --plugin=stringplugin --plugin=testplugin \
   $WHAT
 
-echo "+++++"
+# for acthex development
+if /bin/false; then
+  echo "+++++"
 
-WHAT="./tests/acthex_bubblesort_sortenv.hex"
-EXTRA="--plugin=stringplugin"
-EXTRA=""
-EXTRA="$EXTRA --debug"
-EXTRA="$EXTRA --verbose"
-acthex $EXTRA \
-  --pluginpath=./plugins/ \
-  --plugin=acthex-testplugin \
-  $WHAT
+  WHAT="./tests/acthex_bubblesort_sortenv.hex"
+  EXTRA="--plugin=stringplugin"
+  EXTRA=""
+  EXTRA="$EXTRA --debug"
+  EXTRA="$EXTRA --verbose"
+  acthex $EXTRA \
+    --pluginpath=./plugins/ \
+    --plugin=acthex-testplugin \
+    $WHAT
 
-echo "====="
+  echo "====="
 
-WHAT="./tests/acthex_bubblesort_persistenceenv.hex"
-EXTRA="--plugin=stringplugin"
-EXTRA=""
-EXTRA="$EXTRA --debug"
-EXTRA="$EXTRA --verbose"
-acthex $EXTRA \
-  --pluginpath=./plugins/ \
-  --plugin=acthex-testplugin \
-  $WHAT
-  
+  WHAT="./tests/acthex_bubblesort_persistenceenv.hex"
+  EXTRA="--plugin=stringplugin"
+  EXTRA=""
+  EXTRA="$EXTRA --debug"
+  EXTRA="$EXTRA --verbose"
+  acthex $EXTRA \
+    --pluginpath=./plugins/ \
+    --plugin=acthex-testplugin \
+    $WHAT
+    
+fi
