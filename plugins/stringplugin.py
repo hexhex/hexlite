cf8736c514b5473b68bdf14e7fb02b1d56df0374
Peter Schueller <contact@peterschueller.com>
2019-12-30 13:22:48 +0100

    bugfix in string plugin

--- plugins/stringplugin.py
+++ plugins/stringplugin.py
@@ -1,12 +1,12 @@
 import dlvhex
 
 def concat(strs):
-  needquote = any(['"' in s for s in strs])
-  unquoted = [s.strip('"') for s in strs]
+  needquote = any(['"' in s.value() for s in strs])
+  unquoted = [s.value().strip('"') for s in strs]
   result = ''.join(unquoted)
   if needquote:
     result = '"'+result+'"'
-  dlvhex.output((result,))
+  dlvhex.output( (dlvhex.storeConstant(result),) )
 
 def register():
 	prop = dlvhex.ExtSourceProperties()
