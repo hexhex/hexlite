78ad824ad35c9c263e9714dd3ab2a189c702e237
Peter Schueller <contact@peterschueller.com>
2020-04-06 15:50:48 +0200

    making API more similar to python API
    then we can just pass over the arguments and it will hopefully work

--- java-api/src/main/java/at/ac/tuwien/kr/hexlite/api/ISolverContext.java
+++ java-api/src/main/java/at/ac/tuwien/kr/hexlite/api/ISolverContext.java
@@ -1,12 +1,15 @@
 package at.ac.tuwien.kr.hexlite.api;
 
-import java.util.AbstractSet;
+import java.util.ArrayList;
+import java.util.HashSet;
 
 public interface ISolverContext {
-    public ISymbol storeOutputAtom(IPluginAtom atom);
-    public ISymbol storeAtom(IPluginAtom atom);
+    // otuple = the output tuple of the current evaluation that shall be represented as an ISymbol
+    public ISymbol storeOutputAtom(ArrayList<ISymbol> otuple);
+    // tuple = predicate + arguments of an atom to be represented as an ISymbol
+    public ISymbol storeAtom(ArrayList<ISymbol> tuple);
     public ISymbol storeConstant(String s);
     public ISymbol storeString(String s);
     public ISymbol storeInteger(Integer i);
-    public void learn(AbstractSet<ISymbol> nogood);
+    public void learn(HashSet<ISymbol> nogood);
 }
