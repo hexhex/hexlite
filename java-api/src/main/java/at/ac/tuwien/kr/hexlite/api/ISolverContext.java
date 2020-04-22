02632e3a8f1a3076db2cd201ffbbadb7e2c81bc1
Peter Schueller <schueller.p@gmail.com>
2020-04-22 13:55:20 +0200

    adding an exception to the interface that indicates that a requested atom does not exist in the solver

--- java-api/src/main/java/at/ac/tuwien/kr/hexlite/api/ISolverContext.java
+++ java-api/src/main/java/at/ac/tuwien/kr/hexlite/api/ISolverContext.java
@@ -4,10 +4,15 @@ import java.util.ArrayList;
 import java.util.HashSet;
 
 public interface ISolverContext {
+    public static class StoreAtomException extends Exception {
+        public StoreAtomException(String message) {
+            super(message);
+        }
+    }
     // otuple = the output tuple of the current evaluation that shall be represented as an ISymbol
-    public ISymbol storeOutputAtom(ArrayList<ISymbol> otuple);
+    public ISymbol storeOutputAtom(ArrayList<ISymbol> otuple) throws StoreAtomException;
     // tuple = predicate + arguments of an atom to be represented as an ISymbol
-    public ISymbol storeAtom(ArrayList<ISymbol> tuple);
+    public ISymbol storeAtom(ArrayList<ISymbol> tuple) throws StoreAtomException;
     public ISymbol storeConstant(String s);
     public ISymbol storeString(String s);
     public ISymbol storeInteger(Integer i);
