c38c771b1b623863f9222b1ea5e3f04a61536a63
Peter Schueller <peter@peterschueller.com>
2021-05-13 13:50:47 +0200

    linting

--- setup.py
+++ setup.py
@@ -5,23 +5,26 @@ import platform
 import setuptools
 
 if platform.python_version().startswith('2'):
-  sys.stdout.write("Please use Python 3 instead of Python 2!\n")
-  sys.exit(-1)
+    sys.stdout.write("Please use Python 3 instead of Python 2!\n")
+    sys.exit(-1)
+
 
 def readme():
-  with open('README', 'wb') as of:
-    with open('README.md', 'rb') as i:
-      out = b''.join([ line for line in i if not line.startswith(b'[')])
-      of.write(out)
-      return out.decode('utf8')
+    with open('README', 'wb') as of:
+        with open('README.md', 'rb') as i:
+            out = b''.join([line for line in i if not line.startswith(b'[')])
+            of.write(out)
+            return out.decode('utf8')
+
 
 readme_txt = readme()
-setuptools.setup(name='hexlite',
-      version='1.4.0',
-      description='Hexlite - Solver for a fragment of HEX',
-      long_description=readme_txt,
-      long_description_content_type='text/markdown',
-      classifiers=[
+setuptools.setup(
+    name='hexlite',
+    version='1.4.0',
+    description='Hexlite - Solver for a fragment of HEX',
+    long_description=readme_txt,
+    long_description_content_type='text/markdown',
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python :: 3',
         'Programming Language :: Java',
@@ -34,21 +37,21 @@ setuptools.setup(name='hexlite',
         'Topic :: Scientific/Engineering :: Information Analysis',
         'Topic :: Scientific/Engineering :: Mathematics',
         'Topic :: Utilities',
-      ],
-      url='https://github.com/hexhex/hexlite',
-      author='Peter Schueller',
-      author_email='schueller.p@gmail.com',
-      license='MIT License',
-      packages=['hexlite', 'hexlite.ast', 'dlvhex', 'acthex'],
-      install_requires=[
+    ],
+    url='https://github.com/hexhex/hexlite',
+    author='Peter Schueller',
+    author_email='schueller.p@gmail.com',
+    license='MIT License',
+    packages=['hexlite', 'hexlite.ast', 'dlvhex', 'acthex'],
+    install_requires=[
         'ply>=3.11',
         'clingo>=5.5.0',
         'jpype1>=1.2.1'
-      ],
-      entry_points={
+    ],
+    entry_points={
         'console_scripts': [
-          'hexlite=hexlite.main:main',
-          'acthex=acthex.main:main',
+            'hexlite=hexlite.main:main',
+            'acthex=acthex.main:main',
         ],
-      },
-      zip_safe=False)
+    },
+    zip_safe=False)
